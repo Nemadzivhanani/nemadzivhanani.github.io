1efab1d5954c8364341cd0281535b0ba50bb15f1
Fugza <134562393+Fugza@users.noreply.github.com>
2026-02-06 14:10:50 +0200

    Add Ndivhuho Electrical Works demo page with styles and scripts

---
+++ demos/js/lectrical.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Ndivhuho Electrical Works Demo Loaded");
+    
+    // Future Logic: If we add a Mobile Menu later, we put the code here.
+    // For now, the site is simple enough to not need complex JS.
+});
